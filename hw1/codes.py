c0d2e9762fc8b4f044582edc66cbe2c1562f03d4
Zhaoqi Liu <1196286235@qq.com>
2022-09-09 20:23:42 +0800

    Signed-off-by: Zhaoqi Liu <1196286235@qq.com>

--- hw1/codes.py
+++ hw1/codes.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-N=[10,100,1000,10000,100000,1000000,10000000]
+N=[10,100,1000,10000,100000]
 nlist=np.array(N)
 ilist=np.array([])
 dlist=np.array([])
@@ -8,12 +8,13 @@ olist=np.array([])
 
 
 for n in nlist:
-    x=np.random.rand(n)
-    y=np.sin(x)
-    i=sum(y)/n
-    d=abs(i-(1-np.cos(1)))
-    ilist=np.append(ilist,i)
-    dlist=np.append(dlist,d)
+    d=0
+    for k in range(100):
+        x=np.random.rand(n)
+        y=np.sin(x)
+        i=sum(y)/n
+        d+=abs(i-(1-np.cos(1)))
+    dlist=np.append(dlist,d/100)
 
 olist=np.log(dlist[0:-1]/dlist[1:])/np.log(nlist[1:]/nlist[0:-1])
 print('order:',olist)
